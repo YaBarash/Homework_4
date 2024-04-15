027354f1ca250d260ad5f50a1ce9b813796fff15
Alina Baranova <alina_nemo@mail.ru>
2024-04-15 16:15:33 +0300

    changed names function 1

--- str_func.py
+++ str_func.py
@@ -1,5 +1,9 @@
-def func(string):
+def func_upper(string):
     '''Функция для возврата строки заглавными буквами'''
     return string.upper()
+def func_title(string):
+    '''Функция для возврата строки заглавными буквами'''
+    return string.title()
+
 string = input().lower()
 
