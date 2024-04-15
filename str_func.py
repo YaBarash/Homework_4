21978a30410187250945acc4bca20e006b992f66
Alina Baranova <alina_nemo@mail.ru>
2024-04-15 16:00:42 +0300

    сделала раньше, чем прочитала задание))))) наработки 2 программиста отразились на мне

--- str_func.py
+++ str_func.py
@@ -1,4 +1,9 @@
 def func(string):
+    '''Фнукция возвращает строку заглавными буквами'''
     return string.upper()
+def func_title(string):
+    '''Фнукция возвращает строку с заглавной буквы'''
+    return string.title()
+
 string = input().lower()
 
